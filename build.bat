29cd99f24829c6642f839051249df938bfc9733f
user <user@machine.lan>
2019-09-07 18:15:58 -0400

    Created Build.bat

---
+++ build.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+set MATLAB_BASE=C:\Program Files\MATLAB
+set MATLAB_VER=R2018b
+
+cd "%~dp0"
+:: -batch command added in R2019a.
+call "%MATLAB_BASE%\%MATLAB_VER%\bin\matlab.exe" -wait -sd "%~dp0" -logfile "%~dp0\output.log" -r "cd('%~dp0');exit;"
