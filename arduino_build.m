50ada36c3fd2f172f74e9e62024eaaf667809b40
AutomotiveDevops <automotivedevops@null.com>
2021-02-26 11:22:12 -0500

    Simplified build script. Added close_model to eliminate a hanging dialog box.

--- arduino_build.m
+++ arduino_build.m
@@ -1,15 +1,3 @@
-%% Print Jenkins Environment for Log
-% Jenkins Set Environment Variables
-% https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-below
-envs = {'BUILD_NUMBER', 'BUILD_ID', 'BUILD_URL', 'NODE_NAME', 'JOB_NAME', 'BUILD_TAG', 'JENKINS_URL', 'EXECUTOR_NUMBER', 'WORKSPACE'};
-% Alphabetically sort the environmental variables to print.
-envs = sort(envs);
-% Loop through the variables and print their result to the diary.
-for env = envs
-    env = env{1};
-    fprintf('%s: %s\n', env, getenv(env))
-end
-
 %% Build Process
 bdclose('all');
 model = 'arduino_BasicModel_ci';
@@ -17,14 +5,12 @@ model = 'arduino_BasicModel_ci';
 open_system(model);
 % 
 cs = getActiveConfigSet(model);
-cs.set_param('GenCodeOnly', 'off'); 
+cs.set_param('GenCodeOnly', getenv('GEN_CODE_ONLY')); 
 % Build the model
 slbuild(model);
 
-
-%% Exit Matlab
-% Don't exit Matlab if BUILD_NUMBER is not set.
-% For testing of script outside of Jenkins environment.
-if ~isempty(getenv('BUILD_NUMBER'))
-    exit(0);
-end
+% Save the system and close it.
+% Saves are not pushed back to the version control but it eliminates a
+% dialog prompt that pauses the build process.
+save_system(hdl);
+close_system(hdl, 0);
